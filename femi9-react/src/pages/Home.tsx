723b9723d3fabd44cbb53dda3c68c77d4c963a08
Aaron-Samuel05 <157136322+Aaron-Samuel05@users.noreply.github.com>
2026-09-13 00:13:37 +0530

    Restructure homepage into a product-first editorial flow

--- femi9-react/src/pages/Home.tsx
+++ femi9-react/src/pages/Home.tsx
@@ -5,14 +5,12 @@ import { Story } from '../components/Story'
 import { Impact } from '../components/Impact'
 import { Cta } from '../components/Cta'
 import { ImmersiveShowcase } from '../immersive/ImmersiveShowcase'
-import { Testimonials } from '../immersive/Testimonials'
 import { Journal } from '../components/Journal'
 
 export function Home() {
   return (
     <main id="top">
       <Hero />
-      <Testimonials />
       <Products />
       <ImmersiveShowcase />
       <WhyBento />
